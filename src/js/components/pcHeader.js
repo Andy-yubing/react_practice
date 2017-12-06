8d79a30841755c7ebab748ba5c631e0d4042e599
yubing <29988076@qq.com>
2017-12-07 00:07:15 +0800

    ..

--- src/js/components/pcHeader.js
+++ src/js/components/pcHeader.js
@@ -1,7 +1,12 @@
 import  React from 'react';
 import { Row, Col } from 'antd';
-
+import { Menu, Icon } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 export default class pcHeader extends React.Component{
+  state = {
+    current: 'mail',
+  }
   render(){
     return(
       <header>
---
+++ src/js/components/pcIndex.js
@@ -0,0 +1,11 @@
+import  React from 'react';
+import PcHeader from './pcHeader';
+export default class pcIndex extends React.Component{
+  render(){
+    return(
+        <div>
+            <PcHeader/>
+        </div>
+    )
+  }
+}
